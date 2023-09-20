f9e0e2369cdda9705637928b99906fec29b1730c
RyanTF313 <r.tillman.french@gmail.com>
2023-09-19 21:29:09 -0400

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const jsxViewEngine = require("jsx-view-engine");
+const { pokemon } = require("./models/pokemon");
+
 const app = express();
 const port = 3005;
-const { pokemon } = require("./models/pokemon");
+
+app.set("view engine", "jsx");
+app.engine("jsx", jsxViewEngine());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the pokemon app!");
 });
 
 app.get("/pokemon", (req, res) => {
-  res.send(pokemon);
+  res.render("Index", { pokemon });
 });
 
 app.listen(port, () => {
---
+++ views/Index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const myStyle = {
+  color: "#ffffff",
+  backgroundColor: "#000000",
+};
+
+function Index({ pokemon }) {
+  return (
+    <div style={myStyle}>
+      <h1>Here are some pokemon:</h1>
+    </div>
+  );
+}
+
+export default Index;
