3261c29548281f94b75027e7043b7730c02b4d6e
RyanTF313 <r.tillman.french@gmail.com>
2023-09-19 21:25:24 -0400

    Db is connected and can see json in browser

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3005
+const port = 3005;
+const { pokemon } = require("./models/pokemon");
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the pokemon app!')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the pokemon app!");
+});
 
-app.listen(port, ()=>{
-    console.log('Listening on port ' + port)
-})
+app.get("/pokemon", (req, res) => {
+  res.send(pokemon);
+});
+
+app.listen(port, () => {
+  console.log("Listening on port " + port);
+});
